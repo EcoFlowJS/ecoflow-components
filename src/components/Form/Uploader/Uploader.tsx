d3f336ea0f674d7b2aaebef5cdf8162d6b437117
RomelSikdar <romels500@gmail.com.com>
2024-05-02 22:40:30 +0530

    component update

--- src/components/Form/Uploader/Uploader.tsx
+++ src/components/Form/Uploader/Uploader.tsx
@@ -1,65 +1,133 @@
-import { Input, InputGroup } from "rsuite";
-import { Icon } from "@rsuite/icons";
+import { Button, Input, InputGroup } from "rsuite";
 import { GrDocumentZip } from "react-icons/gr";
 import "./style.less";
-import React, { useEffect } from "react";
+import React, {
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+  ReactNode,
+  useEffect,
+} from "react";
+import IconWrapper from "../../IconWrapper/IconWrapper";
 
-interface UploaderProps {
+interface UploaderProps
+  extends Omit<
+    DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>,
+    "value" | "name" | "onClick" | "onChange" | "rtl"
+  > {
   name?: string;
   value?: FileList | null;
-  onCLick?: React.MouseEventHandler<HTMLInputElement>;
-  onChange?: any;
+  hideTextArea?: boolean;
+  buttonText?: string;
+  icon?: ReactNode;
   rtl?: boolean;
-  [key: string]: any;
+  onClick?: React.MouseEventHandler<HTMLInputElement>;
+  onChange?: (value: FileList | null) => void;
+  onCancel?: () => void;
 }
 
 export default function Uploader({
-  onClick = () => {},
   name,
   value = null,
-  onChange = () => {},
+  hideTextArea = false,
+  buttonText,
+  icon,
   rtl = false,
+  onChange = () => {},
+  onClick = () => {},
   ...props
 }: UploaderProps) {
   const [uploadvalue, setValue] = React.useState("Select File : ");
-  const inputRef = React.useRef(null);
+  const [inputvalue, setInputValue] = React.useState("");
+  const inputRefTextArea = React.useRef(null);
+  const inputRefNoTextArea = React.useRef(null);
+
+  const fileNameList = (files: FileList): string => {
+    let result = "";
+    for (let i = 0; i < files.length; i++) {
+      result = `${result} ${files.item(i)?.name},`;
+    }
+    return result;
+  };
 
   useEffect(() => {
     if (value !== null) {
       setValue(value[0].name);
-      (inputRef.current as any).files = value;
+      if (hideTextArea) (inputRefNoTextArea.current as any).files = value;
+      else (inputRefTextArea.current as any).files = value;
+      return;
     }
-  }, []);
+    setInputValue("");
+    setValue("Select File : ");
+  }, [value]);
 
   return (
-    <InputGroup>
-      <Input
-        readOnly
-        value={uploadvalue}
-        style={{ textAlign: rtl ? "right" : "left", cursor: "pointer" }}
-        onClick={(event) => {
-          (inputRef.current as any).click();
-          onClick(event);
-        }}
-      />
-      <input
-        className="hideInput"
-        type="file"
-        ref={inputRef}
-        name={name}
-        onChange={(event) => {
-          setValue(
-            event.target.files?.length && event.target.files.length >= 1
-              ? event.target.files[0].name
-              : "Select File : "
-          );
-          onChange(event.target.files);
-        }}
-        {...props}
-      />
-      <InputGroup.Button onClick={() => (inputRef.current as any).click()}>
-        <Icon as={GrDocumentZip} />
-      </InputGroup.Button>
-    </InputGroup>
+    <>
+      {!hideTextArea ? (
+        <InputGroup>
+          <Input
+            readOnly
+            value={uploadvalue}
+            style={{ textAlign: rtl ? "right" : "left", cursor: "pointer" }}
+            onClick={(event) => {
+              (inputRefTextArea.current as any).click();
+              onClick(event);
+            }}
+          />
+          <input
+            className="hideInput"
+            type="file"
+            ref={inputRefTextArea}
+            value={inputvalue}
+            name={name}
+            onChange={(event) => {
+              setValue(
+                event.target.files?.length && event.target.files.length > 0
+                  ? event.target.files.length > 1
+                    ? fileNameList(event.target.files)
+                    : event.target.files[0].name
+                  : "Select File : "
+              );
+              onChange(event.target.files);
+              setInputValue(event.target.value);
+            }}
+            {...props}
+          />
+          <InputGroup.Button
+            onClick={() => (inputRefTextArea.current as any).click()}
+            startIcon={icon ? icon : <IconWrapper icon={GrDocumentZip} />}
+          >
+            {buttonText}
+          </InputGroup.Button>
+        </InputGroup>
+      ) : (
+        <>
+          <input
+            className="hideInput"
+            type="file"
+            ref={inputRefNoTextArea}
+            value={inputvalue}
+            name={name}
+            onChange={(event) => {
+              setValue(
+                event.target.files?.length && event.target.files.length > 0
+                  ? event.target.files.length > 1
+                    ? fileNameList(event.target.files)
+                    : event.target.files[0].name
+                  : "Select File : "
+              );
+              onChange(event.target.files);
+              setInputValue(event.target.value);
+            }}
+            {...props}
+          />
+          <Button
+            onClick={() => (inputRefNoTextArea.current as any).click()}
+            startIcon={icon ? icon : <IconWrapper icon={GrDocumentZip} />}
+          >
+            {buttonText}
+          </Button>
+        </>
+      )}
+    </>
   );
 }
